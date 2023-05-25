0c1359f6d8820edea6ae4e02aed2c5f8d5553c45
Mattia Leonori <mattia.leonori@iit.it>
2023-05-25 10:57:09 +0200

    Added Sphinx documentation

---
+++ docs/sphinx/source/conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'xsens_awinda_ros'
+copyright = '2023, Mattia Leonori'
+author = 'Mattia Leonori'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.todo'
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/sphinx/source/kinematic_chain.rst
@@ -0,0 +1,23 @@
+Kinematic Chain
+===============
+
+Kinematics chain
+----------------
+
+#. Step 1. mkdir -p path_to_catkin_ws/src
+
+#. Step 2. cd path_to_catkin_ws/src
+
+#. Step 3. git clone https://github.com/hrii-iit/xsens_awinda_ros.git
+
+#. Step 4. Build your catkin workspace
+cd ..
+catkin build
+
+
+Indexes
+-------
+
+TODO mention hand
+
+- [Kinematics chain: indexes and definition](#definitions)
